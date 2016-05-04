62839c4be67e4f7363725cd5a79aa925e35d5c6a
hideto0710 <h.inamura0710@gmail.com>
2016-05-04 23:00:54 +0900

    delete unique key constraint

--- ci/setup_table.sql
+++ ci/setup_table.sql
@@ -3,6 +3,5 @@ CREATE TABLE account
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(500) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  CONSTRAINT account_mail_address_key UNIQUE (email)
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
